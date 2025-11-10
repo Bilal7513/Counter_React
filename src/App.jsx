9db05a40f9c9beb09ced862aa6ff919780e9af56
Bilal Irfan <bi0755562@gmail.com>
2025-11-10 22:26:19 +0500

    Project Complete

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+ 
+function App() {
+
+  let [counter, setCounter] = useState(5)
+
+  // let counter = 1;
+
+  const addValue = () => {
+    if(counter < 20){
+    setCounter(counter +  1)
+  }
+  }
+
+  const subValue = () => {
+    if(counter > 0){
+      setCounter(counter -  1)
+    }
+  }
+
+  return (
+    <>
+      <h1>Counter Project</h1>
+      <h2>Counter: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <br />
+      <button onClick={subValue}>Sub Value</button>
+      <p>Counter: {counter}</p>
+    </>
+  )
+}
+
+export default App
